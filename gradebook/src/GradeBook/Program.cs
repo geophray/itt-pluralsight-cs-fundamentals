55938a7d247b69ede0f04a61d6173dadacfacd64
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-10 16:08:46 -0600

    Sum scores in numbers array using foreach

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,6 +6,13 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new [] {12.3, 15.78, 44.1};
+            var result = 0.0;
+            foreach(var number in numbers) {
+                result += number;
+            }
+            Console.WriteLine(result);
+
             if (args.Length > 0) {
                 Console.WriteLine($"Hello {args[0]}!");
             } else {

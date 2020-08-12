86bad5da42d2a3a14ce5e78b703fee6ee4ca5033
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-12 12:23:10 -0600

    Add grades from user input

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,14 +8,29 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("My Math Gradebook");
-            book.AddGrade(89.1);
-            book.AddGrade(90.6);
-            book.AddGrade(73.2);
+            
+            var done = false;
+
+            while(!done)
+            {   
+                Console.WriteLine($"Please enter a new grade to add to {book.Name}, or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if(input == "q")
+                {
+                    done = true;
+                    continue;
+                }
+
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+
+            }
 
             var stats = book.GetStatistics();
 
-            Console.WriteLine($"The lowest grade is {stats.Low}");
-            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"The highest grade is {stats.High:N1}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The letter grade is {stats.Letter}");
         }

4ff522884934a1252c24ca1ab29556abf36d20cd
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-13 16:06:55 -0600

    Add DiskBook class

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -19,14 +19,9 @@ namespace GradeBook
         {
         }
 
-        public virtual event GradeAddedDelegate GradeAdded;
-
+        public abstract event GradeAddedDelegate GradeAdded;
         public abstract void AddGrade(double grade);
-
-        public virtual Statistics GetStatistics()
-        {
-            throw new NotImplementedException();
-        }
+        public abstract Statistics GetStatistics();
     }
 
     public class InMemoryBook : Book

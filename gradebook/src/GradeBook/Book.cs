ca512d320e7c9af3b445225ea36d20d9d353fa73
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-13 16:21:15 -0600

    Move calculations inside Statistics class

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -79,38 +79,10 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
 
-            foreach(var grade in grades) {
-                result.Average += grade;
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-            }
-            result.Average /= grades.Count;
-
-            switch(result.Average)
+            for(var index = 0; index < grades.Count; index += 1) 
             {
-                case var d when d > 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d > 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d > 70.0:
-                    result.Letter = 'C';
-                    break;
-
-                case var d when d > 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                default:
-                    result.Letter = 'F';
-                    break;
+                result.Add(grades[index]);
             }
 
             return result;
